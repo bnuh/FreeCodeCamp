4fdc0a822a22cde854395d12347a83875de77b35
bnuh <bryan.melanson@gmail.com>
2017-02-25 18:14:34 -0330

    Working JSONP and search bar implemented

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
-});
+            var api = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=";
+            var title = "war";
+            var suff = "&callback=?";
+            $.getJSON((api + title + suff),
+                function(json) {
+                    console.log(json);
+                });
+        });
