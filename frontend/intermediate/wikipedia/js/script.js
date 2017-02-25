dd12bfff0d44cb65492eb955e9ed3ce0f65b95be
bnuh <bryan.melanson@gmail.com>
2017-02-25 18:40:50 -0330

    JSON query returning array of results

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-            var api = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=";
+            var api = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&generator=search&gsrsearch=meaning";
             var title = "war";
             var suff = "&callback=?";
-            $.getJSON((api + title + suff),
+            $.getJSON((api + suff),
                 function(json) {
                     console.log(json);
                 });
