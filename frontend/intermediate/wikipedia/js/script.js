3f23fdbb0da9a46a367d52149ac7041a26817ef2
brymel <bryan.melanson@gmail.com>
2017-03-01 13:04:29 -0330

    Browser opens URL if single page result

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-            var api = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow&generator=search&gsrsearch=Stack%20Overflow&";
+            var api = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow";
+            // if length of array is 1, go to this location, if -1, run 2nd search
+            var api2 = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&generator=search&gsrsearch=Stack%20Overflow&";
+
             var title = "war";
             var suff = "&callback=?";
             $.getJSON((api + suff),
@@ -10,6 +13,14 @@ $(document).ready(function() {
                       arr.push(key);
                   }
                   len = arr.length;
+                  if (len == 1) {
+                    if (arr[0] == 1) {
+                      var api2 = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&generator=search&gsrsearch=Stack%20Overflow&";
+                    }
+                    else {
+                      window.location.href = 'http://www.google.com';
+                    }
+                  }
                   for (var i = 0; i < len; i++){
                     console.log(json.query.pages[arr[i]].title);
                   }
