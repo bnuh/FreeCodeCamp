0da68ef5600e7baead4f1a560155e436d7b3ae0a
brymel <bryan.melanson@gmail.com>
2017-02-28 22:22:11 -0330

    API call returns intro for search display

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-            var api = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&generator=search&gsrsearch=meaning";
+            var api = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow&generator=search&gsrsearch=Stack%20Overflow&";
             var title = "war";
             var suff = "&callback=?";
             $.getJSON((api + suff),
                 function(json) {
+                  console.log(json);
                   var arr = [], len;
                   for(key in json.query.pages) {
                       arr.push(key);
