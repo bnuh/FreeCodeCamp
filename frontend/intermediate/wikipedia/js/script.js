6fcb7b5ccf8bc76d2a106cb9e78554c929e0b0c1
brymel <bryan.melanson@gmail.com>
2017-03-01 22:58:46 -0330

    Divs and API formatting

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,28 +1,51 @@
 $(document).ready(function() {
-            var api = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&titles=Stack%20Overflow";
-            // if length of array is 1, go to this location, if -1, run 2nd search
-            var api2 = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&generator=search&gsrsearch=Stack%20Overflow&";
+    var go = "http://en.wikipedia.org/w/api.php?action=query&format=json&inprop=url&exintro=1&titles=";
+    var search = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=search&exchars=175&exlimit=max&exintro=1&inprop=url&gsrsearch=";
+    var title = "";
+    var suff = "&callback=?";
 
-            var title = "war";
-            var suff = "&callback=?";
-            $.getJSON((api + suff),
-                function(json) {
-                  console.log(json);
-                  var arr = [], len;
-                  for(key in json.query.pages) {
-                      arr.push(key);
-                  }
-                  len = arr.length;
-                  if (len == 1) {
-                    if (arr[0] == 1) {
-                      var api2 = "http://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&exintro=&explaintext=&generator=search&gsrsearch=Stack%20Overflow&";
+    $("#go").click(function() {
+        title = $(".search-query").val();
+        $.getJSON((go + title + suff),
+            function(json) {
+                var arr = [], len;
+                for (key in json.query.pages) {
+                    arr.push(key);
+                }
+                len = arr.length;
+                if (len == 1) {
+                    if (arr[0] != -1) {
+                      window.location.href = json.query.pages[arr[0]].fullurl;
                     }
                     else {
-                      window.location.href = 'http://www.google.com';
+                      console.log("Provide error page");
                     }
-                  }
-                  for (var i = 0; i < len; i++){
-                    console.log(json.query.pages[arr[i]].title);
-                  }
-                });
-        });
+                }
+                for (var i = 0; i < len; i++) {
+                  console.log(json.query.pages[arr[i]].extract);
+                  console.log(json.query.pages[arr[i]].fullurl);
+                  console.log(json.query.pages[arr[i]].title);
+                }
+            });
+    });
+
+    $("#search").click(function() {
+        title = $(".search-query").val();
+        $.getJSON((search + title + suff),
+            function(json) {
+              console.log(json);
+                var arr = [], len;
+                for (key in json.query.pages) {
+                    arr.push(key);
+                }
+                len = arr.length;
+                for (var i = 0; i < len; i++) {
+                  console.log(json.query.pages[arr[i]].extract);
+                  console.log(json.query.pages[arr[i]].fullurl);
+                  console.log(json.query.pages[arr[i]].title);
+                }
+            });
+    });
+
+
+});
