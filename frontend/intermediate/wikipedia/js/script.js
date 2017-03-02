2dd153667dcd41e236bf3b0cd48789c8f56daae0
brymel <bryan.melanson@gmail.com>
2017-03-02 01:17:55 -0330

    Results displaying properly

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     var go = "http://en.wikipedia.org/w/api.php?action=query&format=json&inprop=url&exintro=1&titles=";
-    var search = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=search&exchars=175&exlimit=max&exintro=1&inprop=url&gsrsearch=";
+    var search = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=search&exchars=150&explaintext&exlimit=max&exintro=1&inprop=url&gsrsearch=";
     var title = "";
     var suff = "&callback=?";
+    var list = document.getElementById('results');
+    var head = "";
+    var summ = "";
 
     $("#go").click(function() {
         title = $(".search-query").val();
@@ -21,11 +24,6 @@ $(document).ready(function() {
                       console.log("Provide error page");
                     }
                 }
-                for (var i = 0; i < len; i++) {
-                  console.log(json.query.pages[arr[i]].extract);
-                  console.log(json.query.pages[arr[i]].fullurl);
-                  console.log(json.query.pages[arr[i]].title);
-                }
             });
     });
 
@@ -33,16 +31,13 @@ $(document).ready(function() {
         title = $(".search-query").val();
         $.getJSON((search + title + suff),
             function(json) {
-              console.log(json);
                 var arr = [], len;
                 for (key in json.query.pages) {
                     arr.push(key);
                 }
                 len = arr.length;
                 for (var i = 0; i < len; i++) {
-                  console.log(json.query.pages[arr[i]].extract);
-                  console.log(json.query.pages[arr[i]].fullurl);
-                  console.log(json.query.pages[arr[i]].title);
+                  $('#results').append("<li><h1><a href='" + json.query.pages[arr[i]].url + "'>"+ json.query.pages[arr[i]].title + "</h1><p>" + json.query.pages[arr[i]].extract + "</a></p></li>");
                 }
             });
     });
