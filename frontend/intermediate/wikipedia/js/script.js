1491407420bea91fd00d5feeb060693692e8c652
brymel <bryan.melanson@gmail.com>
2017-03-02 11:20:46 -0330

    Removing "Go" button

--- frontend/intermediate/wikipedia/js/script.js
+++ frontend/intermediate/wikipedia/js/script.js
@@ -1,31 +1,10 @@
 $(document).ready(function() {
-    var go = "http://en.wikipedia.org/w/api.php?action=query&format=json&inprop=url&exintro=1&titles=";
     var search = "http://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&generator=search&exchars=150&explaintext&exlimit=max&exintro=1&inprop=url&gsrsearch=";
     var title = "";
     var suff = "&callback=?";
     var list = document.getElementById('results');
     var head = "";
     var summ = "";
-
-    $("#go").click(function() {
-        title = $(".search-query").val();
-        $.getJSON((go + title + suff),
-            function(json) {
-                var arr = [], len;
-                for (key in json.query.pages) {
-                    arr.push(key);
-                }
-                len = arr.length;
-                if (len == 1) {
-                    if (arr[0] != -1) {
-                      window.location.href = json.query.pages[arr[0]].fullurl;
-                    }
-                    else {
-                      console.log("Provide error page");
-                    }
-                }
-            });
-    });
 
     $("#search").click(function() {
         title = $(".search-query").val();
