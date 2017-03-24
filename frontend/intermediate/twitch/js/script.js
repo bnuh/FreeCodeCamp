8ee3d9395078592f1c826c19ce9d9595871d2b5f
bnuh <bryan.melanson@gmail.com>
2017-03-24 16:41:40 -0230

    Display avatars for offline accounts

--- frontend/intermediate/twitch/js/script.js
+++ frontend/intermediate/twitch/js/script.js
@@ -1,15 +1,14 @@
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 users.sort().forEach(function(user) {
-            $(document).ready(function() {
-                  $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?', function(data) {
-                      console.log(data);
-                      if (data.stream != null){
-                          $('.streams').append("<li><img class='logo' src=" + data.stream.channel.logo + "></li>");
-                      }
-                      else {
-                          $('.streams').append("<li><img class='logo' src='https://dummyimage.com/100x100/98c4f0/ffffff.jpg&text=Offline'></li>");
-                      }
-                  });
-                });
-            });
+    $(document).ready(function() {
+        $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data) {
+            console.log(data.logo);
+            if (data.logo != null || data.logo != undefined) {
+                $('.streams').append("<li><img class='logo' src=" + data.logo + "></li>");
+            } else {
+                $('.streams').append("<li><img class='logo' src='https://dummyimage.com/100x100/98c4f0/ffffff.jpg&text=Offline'></li>");
+            }
+        });
+    });
+});
