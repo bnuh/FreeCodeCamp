e4a2c21dcbe92ee7252a3b85c3ccf89c5860b084
bnuh <bryan.melanson@gmail.com>
2017-03-24 16:51:25 -0230

    Remove "not found" users

--- frontend/intermediate/twitch/js/script.js
+++ frontend/intermediate/twitch/js/script.js
@@ -3,10 +3,14 @@ var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "
 users.sort().forEach(function(user) {
     $(document).ready(function() {
         $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data) {
-            console.log(data.logo);
+            console.log(data);
             if (data.logo != null || data.logo != undefined) {
                 $('.streams').append("<li><img class='logo' src=" + data.logo + "></li>");
-            } else {
+            }
+            else if (data.status == "404"){
+
+            }
+            else {
                 $('.streams').append("<li><img class='logo' src='https://dummyimage.com/100x100/98c4f0/ffffff.jpg&text=Offline'></li>");
             }
         });
