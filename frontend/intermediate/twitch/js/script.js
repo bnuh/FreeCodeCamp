afc243e222a525f80bcd256a439917cdbe6e43f1
bnuh <bryan.melanson@gmail.com>
2017-03-26 14:48:26 -0230

    Font choices

--- frontend/intermediate/twitch/js/script.js
+++ frontend/intermediate/twitch/js/script.js
@@ -1,4 +1,4 @@
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 var logo = 'https://dummyimage.com/100x100/c1cfe8/ffffff&text=OFFLINE';
 var status = '';
 var game = '';
@@ -17,7 +17,7 @@ $(document).ready(function() {
                 url = str.stream.channel.url;
                 html = "<div class='row streamer'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'><img class='logo' src=" +
                 logo + "></div><div class='summary col-lg-10 col-md-8 col-sm-8 col-xs-10'><div class='row name'><a href='http://www.twitch.tv/" +
-                user + "'>" + name + "</a></div><div class=row status'>" + status + "</div><div class='row game'>" + game +
+                user + "'>" + name + "</a></div><div class='row status'>" + status + "</div><div class='row stream'>" + game +
                 "</div></div></div>";
                 $('.streamers').prepend(html);
             } else {
@@ -27,7 +27,7 @@ $(document).ready(function() {
                 name = user;
                 html = "<div class='row streamer'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'><img class='logo' src=" +
                 logo + "></div><div class='summary col-lg-10 col-md-8 col-sm-8 col-xs-10'><div class='row name'><a href='http://www.twitch.tv/" +
-                user + "'>" + name + "</a></div><div class=row status'>" + status + "</div><div class='row game'>" + game +
+                user + "'>" + name + "</a></div><div class='row status'>" + status + "</div><div class='row stream'>" + game +
                 "</div></div></div>";
                 $('.streamers').append(html);
             }
