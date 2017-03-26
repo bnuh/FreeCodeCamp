d3b9b7b99b894cbabe1bad0a2b8f79bb8d6a6107
bnuh <bryan.melanson@gmail.com>
2017-03-26 13:57:54 -0230

    Clickable usernames

--- frontend/intermediate/twitch/js/script.js
+++ frontend/intermediate/twitch/js/script.js
@@ -8,24 +8,28 @@ var html = '';
 $(document).ready(function() {
     users.sort().forEach(function(user) {
         $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?', function(str) {
-            if (str.stream != null){
+            console.log(str);
+            if (str.stream != null) {
                 status = "Online";
                 game = str.stream.channel.game + " - " + str.stream.channel.status;
-                console.log(str.stream.channel.game);
-            }
-            else {
+                logo = str.stream.channel.logo;
+                name = str.stream.channel.display_name;
+                url = str.stream.channel.url;
+                html = "<div class='row streamer'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'><img class='logo' src=" +
+                logo + "></div><div class='summary col-lg-10 col-md-8 col-sm-8 col-xs-10'><div class='row name'><a href='" +
+                url + "'>" + name + "</a></div><div class=row status'>" + status + "</div><div class='row game'>" + game +
+                "</div></div></div>";
+                $('.streamers').prepend(html);
+            } else {
                 status = "Offline";
+                logo = 'https://dummyimage.com/100x100/c1cfe8/ffffff&text=OFFLINE';
                 game = "";
-            }
-        });
-        $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + user + '?callback=?', function(data) {
-            if (data.status != '404'){
-                logo = data.logo;
-                if (data.display_name != undefined) {
-                    name = data.display_name;
-                }
-                html = "<div class='row streamer'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'><img class='logo' src=" + logo + "></div><div class='summary col-lg-10 col-md-8 col-sm-8 col-xs-10'><div class='row name'>" + name +
-                "</div><div class=row status'>" + status + "</div><div class='row game'>" + game + "</div></div></div>";
+                name = user;
+                url = str.stream.channel.url;
+                html = "<div class='row streamer'><div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'><img class='logo' src=" +
+                logo + "></div><div class='summary col-lg-10 col-md-8 col-sm-8 col-xs-10'><div class='row name'><a href='" +
+                url + "'>" + name + "</a></div><div class=row status'>" + status + "</div><div class='row game'>" + game +
+                "</div></div></div>";
                 $('.streamers').append(html);
             }
         });
