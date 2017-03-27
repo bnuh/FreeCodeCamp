d2c8bf185294cd84cafba306a7b00b6a952f2271
bnuh <bryan.melanson@gmail.com>
2017-03-26 21:58:06 -0230

    Solution using Math.min and Math.max

--- algorithms/intermediate/SumAllNumbersInARange.js
+++ algorithms/intermediate/SumAllNumbersInARange.js
@@ -1,5 +1,14 @@
 function sumAll(arr) {
-  
-}
+    var sum = 0;
+      for (var i = Math.min(arr[0],arr[1]); i <= Math.max(arr[0],arr[1]); i++){
+          sum += i;
+      }
+      console.log(sum);
 
-sumAll([1, 4]);
+    return sum;
+}
+sumAll([1,4]);
+sumAll([5,1]);
+
+// We'll pass you an array of two numbers.
+// Return the sum of those two numbers and all numbers between them.
