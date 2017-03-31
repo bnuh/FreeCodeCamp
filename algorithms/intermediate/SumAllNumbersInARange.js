4711cd0d13664bfc2c0ae28bb242c2076f57a079
bnuh <bryan.melanson@gmail.com>
2017-03-30 22:52:26 -0230

    Solution

--- algorithms/intermediate/SumAllNumbersInARange.js
+++ algorithms/intermediate/SumAllNumbersInARange.js
@@ -3,8 +3,6 @@ function sumAll(arr) {
       for (var i = Math.min(arr[0],arr[1]); i <= Math.max(arr[0],arr[1]); i++){
           sum += i;
       }
-      console.log(sum);
-
     return sum;
 }
 sumAll([1,4]);
