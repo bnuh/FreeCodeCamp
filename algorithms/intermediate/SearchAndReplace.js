7108a32e9635e79c404b4dca88c9fa8d4b0dbc41
bnuh <bryan.melanson@gmail.com>
2017-04-03 01:13:40 -0230

    Initial commit

---
+++ algorithms/intermediate/SearchAndReplace.js
@@ -0,0 +1,21 @@
+//Perform a search and replace on the sentence using the arguments provided and
+//return the new sentence.
+//First argument is the sentence to perform the search and replace on.
+//Second argument is the word that you will be replacing (before).
+//Third argument is what you will be replacing the second argument with (after).
+
+function myReplace(str, before, after) {
+  return str;
+}
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+myReplace("Let us go to the store", "store", "mall");
+
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
+
+myReplace("This has a spellngi error", "spellngi", "spelling");
+
+myReplace("His name is Tom", "Tom", "john");
+
+myReplace("Let us get back to more Coding", "Coding", "algorithms");
