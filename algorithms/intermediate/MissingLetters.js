6d46313b0e597b282752ca8f6bf6ab7b92bf7dea
bnuh <bryan.melanson@gmail.com>
2017-04-04 17:44:46 -0230

    Initial commit

---
+++ algorithms/intermediate/MissingLetters.js
@@ -0,0 +1,16 @@
+
+//Find the missing letter in the passed letter range and return it.
+//If all letters are present in the range, return undefined.
+
+function fearNotLetter(str) {
+  
+  return str;
+}
+
+fearNotLetter("abce");
+fearNotLetter("abcdefghjklmno");
+// should return "i".
+fearNotLetter("bcd");
+// should return undefined.
+fearNotLetter("yz");
+// should return undefined.
