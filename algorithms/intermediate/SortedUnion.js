b987727ccfff8031059cbb597ba1cc42c024c96f
bnuh <bryan.melanson@gmail.com>
2017-04-05 15:08:32 -0230

    Solution

--- algorithms/intermediate/SortedUnion.js
+++ algorithms/intermediate/SortedUnion.js
@@ -2,15 +2,36 @@
 // of unique values in the order of the original provided arrays.
 // In other words, all values present from all arrays should be included in
 // their original order, but with no duplicates in the final array.
-//The unique numbers should be sorted by their original order, but the final
+// The unique numbers should be sorted by their original order, but the final
 // array should not be sorted in numerical order.
 // Check the assertion tests for examples.
 
-function uniteUnique(arr) {
-  return arr;
+function uniteUnique(arr1, arr2, arr3, arr4) {
+  var newArr = [];
+  for (var i in arr1){
+    if (newArr.indexOf(arr1[i]) == -1){
+      newArr.push(arr1[i]);
+    }
+  }
+  for (var i in arr2){
+    if (newArr.indexOf(arr2[i]) == -1){
+      newArr.push(arr2[i]);
+    }
+  }
+  for (var i in arr3){
+    if (newArr.indexOf(arr3[i]) == -1){
+      newArr.push(arr3[i]);
+    }
+  }
+  for (var i in arr4){
+    if (newArr.indexOf(arr4[i]) == -1){
+      newArr.push(arr4[i]);
+    }
+  }
+  console.log(newArr);
+  return newArr;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 // should return [1, 3, 2, 5, 4].
 uniteUnique([1, 3, 2], [1, [5]], [2, [4]]);
