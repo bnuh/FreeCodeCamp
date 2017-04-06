920979e383587e3bc97930dbfe76edad70d3e689
bnuh <bryan.melanson@gmail.com>
2017-04-06 01:16:23 -0230

    Initial commit

---
+++ algorithms/intermediate/SpinalTapCase.js
@@ -0,0 +1,16 @@
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  return str;
+}
+
+spinalCase("This Is Spinal Tap");
+// should return "this-is-spinal-tap".
+spinalCase("thisIsSpinalTap");
+// should return "this-is-spinal-tap".
+spinalCase("The_Andy_Griffith_Show");
+// should return "the-andy-griffith-show".
+spinalCase("Teletubbies say Eh-oh");
+// should return "teletubbies-say-eh-oh".
