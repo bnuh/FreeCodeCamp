ba28bb5b8545396a4b5fb0785b653284a6979017
bnuh <bryan.melanson@gmail.com>
2017-04-06 02:17:11 -0230

    Solution

--- algorithms/intermediate/SumAllOddFibonacciNumbers.js
+++ algorithms/intermediate/SumAllOddFibonacciNumbers.js
@@ -6,7 +6,19 @@
 //For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 10 are 1, 1, 3, and 5.
 
 function sumFibs(num) {
-  return num;
+  if (num == 2) { return 2 };
+  else if (num == 1) { return 1 };
+  else {
+    var arr = [1, 1];
+    var sum = 2;
+    for (var i = 2; i <= num; i++){
+        arr = [arr[1], (arr[1] + arr[0])];
+        if (arr[1] <= num && arr[1] %2 != 0){
+          sum += arr[1];
+        }
+    }
+    return sum;
+  }
 }
 
 sumFibs(4);
@@ -16,8 +28,6 @@ sumFibs(1000);
 // should return 1785.
 sumFibs(4000000);
 // should return 4613732.
-sumFibs(4);
-// should return 5.
 sumFibs(75024);
 // should return 60696.
 sumFibs(75025);
