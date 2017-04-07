eb557d91163e03cf84b7ae85af47aaf8c899a2be
bnuh <bryan.melanson@gmail.com>
2017-04-07 15:26:20 -0230

    Solution

--- algorithms/intermediate/BinaryAgents.js
+++ algorithms/intermediate/BinaryAgents.js
@@ -2,7 +2,18 @@
 // The binary string will be space separated.
 
 function binaryAgent(str) {
-  return str;
+  var arr = str.split(" ");
+  var str1 = "";
+  for (var i in arr){
+    var sum = 0;
+    var ind = 7;
+    for (var j in arr[i]){
+      sum += (arr[i][j] * Math.pow(2,ind));
+      ind--;
+    }
+    str1 = str1.concat(String.fromCharCode(sum));
+  }
+  return str1;
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
