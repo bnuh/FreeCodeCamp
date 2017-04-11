5886198e2984b5a819eb1cc2697b2ffcb029d23e
bnuh <bryan.melanson@gmail.com>
2017-04-11 00:18:11 -0230

    Solution

--- algorithms/intermediate/ArgumentsOptional.js
+++ algorithms/intermediate/ArgumentsOptional.js
@@ -1,4 +1,5 @@
-// Create a function that sums two arguments together. If only one argument is provided,
+// Create a function that sums two arguments together.
+// If only one argument is provided,
 // then return a function that expects one argument and returns the sum.
 // For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
 // Calling this returned function with a single argument will then return the sum:
@@ -7,18 +8,37 @@
 // If either argument isn't a valid number, return undefined.
 
 function addTogether() {
-  return false;
+    var sum = 0;
+    if (arguments.length == 2) {
+        if (typeof(arguments[0]) != 'number' || typeof(arguments[1]) != 'number') {
+            return undefined;
+        } else {
+            return arguments[0] + arguments[1];
+        }
+    } else if (arguments.length == 1) {
+        if (typeof(arguments[0]) != 'number') {
+            return undefined;
+        } else {
+            var sum1 = arguments[0];
+            return function(x) {
+                if (typeof(arguments[0]) != 'number') {
+                    return undefined;
+                } else {
+                    return sum1 + arguments[0];
+                }
+            }
+        }
+    }
 }
 
-addTogether(2,3);
 
-addTogether(2, 3);
+// console.log(addTogether(2, 3));
 // should return 5.
-addTogether(2)(3);
+// console.log(addTogether(2)(3));
 // should return 5.
-addTogether("http://bit.ly/IqT6zt");
+console.log(addTogether("http://bit.ly/IqT6zt"));
 // should return undefined.
-addTogether(2, "3");
+console.log(addTogether(2, "3"));
 // should return undefined.
-addTogether(2)([3]);
+//addTogether(2)([3]);
 // should return undefined.
