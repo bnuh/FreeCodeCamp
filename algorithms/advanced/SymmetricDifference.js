721f37126f2ca393a6f63df2c6a25b53d240908c
bnuh <bryan.melanson@gmail.com>
2017-05-26 12:55:56 -0230

    Solution

--- algorithms/advanced/SymmetricDifference.js
+++ algorithms/advanced/SymmetricDifference.js
@@ -3,26 +3,32 @@
 //Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
 
 
-function sym(args) {
-	var fin = [];
-	for (var j = 0; j < args.length-1; j++){
-		var temp = comp(arguments[j], arguments[j+1]);
-		fin.concat(temp);
-	}
-	console.log(fin);
-}
+function sym() {
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
 
-function comp(arg1, arg2){
-	var arr = [];
-	// need to account for multiple values in one array
-	for (var i = 0; i < arg1.length; i++){
-		if (arg2.indexOf(arg1[i]) == -1) {
-			console.log(arg2.indexOf(arg1[i]));
-			arr.push(arg1[i]);
-		}
-		console.log(arr);
-	}
-	return arr
+    function symDiff(arrayOne, arrayTwo) {
+        var result = [];
+
+        arrayOne.forEach(function(item) {
+            if (arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        arrayTwo.forEach(function(item) {
+            if (arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        return result;
+    }
+
+    // Apply reduce method to args array, using the symDiff function
+    return args.reduce(symDiff);
 }
 
 // TESTS
