6adb0f976e82644f87e153ee2f84847300b480aa
Bryan Melanson <bryan.melanson@gmail.com>
2017-06-08 16:52:50 -0230

    Updating

--- algorithms/advanced/ExactChange.js
+++ algorithms/advanced/ExactChange.js
@@ -1,6 +1,36 @@
 
 function checkCashRegister(price, cash, cid) {
-  var change;
+  var balance = cash - price;
+  var sum = 0;
+  var change = [];
+  for (var i = 0; i < cid.length; i++){
+    sum += cid[i][1];
+  }
+  if (sum < balance || (sum%balance) !== 0){
+    return "Insufficient Funds";
+  }
+  else if (sum == balance){
+    return "Closed";
+  }
+  while (balance > 0 && sum > 0){
+    var tmp = 0;
+    if (floor(balance/100) > 0){
+      tmp = 0;
+      while(balance > 100 && cid[9][1] !== 0){
+        balance -= 100;
+        tmp += 1;
+      }
+      change.push(["ONE HUNDRED", (100.00 * tmp)]);
+    }
+    if (floor(balance/50) > 0){
+      tmp = 0;
+      while(balance > 50 && cid[8][1] !== 0){
+        balance -= 50;
+        tmp += 1;
+      }
+      change.push(["FIFTY", (50.00 * tmp)]);
+    } 
+  }
   // Here is your change, ma'am.
   return change;
 }
@@ -17,20 +47,3 @@ function checkCashRegister(price, cash, cid) {
 // ["ONE HUNDRED", 100.00]]
 
 checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-// should return an array.
-checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-// should return a string.
-checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-// should return a string.
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-// should return [["QUARTER", 0.50]].
-checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
-// should return [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]].
-checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-// should return "Insufficient Funds".
-checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-// should return "Insufficient Funds".
-checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
-// should return "Closed".
