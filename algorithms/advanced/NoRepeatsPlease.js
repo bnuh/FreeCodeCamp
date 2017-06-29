ffc05d162c726b84dc47c39d760d441cfdd60615
bnuh <bryan.melanson@gmail.com>
2017-06-29 01:20:51 -0230

    Partial solution

--- algorithms/advanced/NoRepeatsPlease.js
+++ algorithms/advanced/NoRepeatsPlease.js
@@ -1,27 +1,54 @@
 // Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
-// For example, aab should retur n 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
+// For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
 
 function permAlone(str) {
-  return str;
+    str = str.split("").sort();
+    count = 1;
+    repeats = [];
+    for (var i = 0; i < str.length; i++){
+        if (str[i] == str[i+1]) { count++; }
+        else { 
+            if (count > 1) repeats.push(count);
+            count = 1;
+        }
+    }
+    var total = fac(str.length);
+    var invalid = fac(str.length-1);
+    for (var i = 0; i < repeats.length; i++){
+        invalid *= fac(repeats[i]);
+    }
+    if (repeats.length > 1){
+        var overlaps = fac(str.length - repeats.length)
+        for (var i = 0; i < repeats.length; i++){
+            overlaps *= fac(repeats[i]);
+        }
+    }
+    else { overlaps = 0 }
+
+    return total - invalid + overlaps;
 }
 
-permAlone("aab");
-// should return a number.
-permAlone("aab");
+function fac(num){
+    total = num
+    for (var i = num; i > 1; i--){ total *= i-1 };
+    return total;
+}
+
+//console.log(permAlone("aab"));
 // should return 2.
-permAlone("aaa");
+//console.log(permAlone("aaa"));
 // should return 0.
-permAlone("aabb");
+//console.log(permAlone("aabb"));
 // should return 8.
-permAlone("abcdefa");
+//console.log(permAlone("abcdefa"));
 // should return 3600.
-permAlone("abfdefa");
+//console.log(permAlone("abfdefa"));
 // should return 2640.
-permAlone("zzzzzzzz");
+//console.log(permAlone("zzzzzzzz"));
 // should return 0.
-permAlone("a");
+//console.log(permAlone("a"));
 // should return 1.
-permAlone("aaab");
+//console.log(permAlone("aaab"));
 // should return 0.
-permAlone("aaabb");
+//console.log(permAlone("aaabb"));
 // should return 12.
