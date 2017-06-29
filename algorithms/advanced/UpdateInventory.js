c6fdc59307d66024f3771dd5ba6d917d3b58ff4c
bnuh <bryan.melanson@gmail.com>
2017-06-28 23:26:21 -0230

    Solution - missed empty array test :finnadie:

--- algorithms/advanced/UpdateInventory.js
+++ algorithms/advanced/UpdateInventory.js
@@ -3,12 +3,10 @@
 function updateInventory(arr1, arr2) {
   var index = arr1.map(function(el){return el[1];});
   var index2 = arr2.map(function(el){return el[1];});
-  for (var i = 0; i < index.length; i++) {
-        if (index.indexOf(index2[i]) == -1) {
-          arr1.push(arr2[i]);
-        }
-        else {
-          arr1[i][0] += arr2[index.indexOf(index2[i])][0];
+  for (var i = 0; i < index2.length; i++) {
+        if (index2[i] != undefined){
+          if (index.indexOf(index2[i]) == -1) { arr1.push(arr2[i]); }
+          else { arr1[i][0] += arr2[index.indexOf(index2[i])][0]; }
         }
     }
     return arr1.sort(function(a, b){return a[1].localeCompare(b[1]);});
@@ -34,9 +32,9 @@ var newInv = [
 // should return an array
 // console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]).length);
 // should return an array with a length of 6.
-console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]));
+// console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]));
 // should return [[88, "Bowling Ball"], [2, "Dirty Sock"], [3, "Hair Pin"], [3, "Half-Eaten Apple"], [5, "Microphone"], [7, "Toothpaste"]].
-// console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], []));
+updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], []);
 // should return [[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]].
 //updateInventory([], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]);
 // should return [[67, "Bowling Ball"], [2, "Hair Pin"], [3, "Half-Eaten Apple"], [7, "Toothpaste"]].
