e0dc492dc301340319a3619ce1e12ac48652df37
bnuh <bryan.melanson@gmail.com>
2017-06-30 02:53:52 -0230

    Solution :boom:

--- algorithms/advanced/MapTheDebris.js
+++ algorithms/advanced/MapTheDebris.js
@@ -4,13 +4,23 @@
 // The values should be rounded to the nearest whole number. The body being orbited is Earth.
 // The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
 
+// Period is 2π * sqrt(axis^3 / GM)
 function orbitalPeriod(arr) {
-  var GM = 398600.4418;
-  var earthRadius = 6367.4447;
-  return arr;
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    var x = [];
+    // axis = radius + altitude
+    for (var i = 0; i < arr.length; i++) {
+        var obj = {
+            name: arr[i].name,
+            orbitalPeriod: Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + arr[i].avgAlt, 3)/GM))
+        };
+        x.push(obj);
+    }
+    return x; 
 }
 
-orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
 // should return [{name: "sputnik", orbitalPeriod: 86400}].
-orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
+console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]));
 // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
